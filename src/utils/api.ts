fdafd2a9b895873f334e25ce47c06487350143a2
juerbau <juerbau@web.de>
2025-12-12 19:20:53 +0100

    Update

---
+++ src/utils/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: 'http://localhost:8000',
+    withCredentials: true,
+
+    xsrfCookieName: 'XSRF-TOKEN',
+    xsrfHeaderName: 'X-XSRF-TOKEN',
+
+    headers: {
+        Accept: 'application/json',
+    },
+});
+
+export default api;
